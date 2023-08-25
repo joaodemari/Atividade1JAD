3616e26a0e98c1485009fb491332b0e04b3f466b
Your Name <you@example.com>
2023-08-25 18:19:07 -0300

    commit final com todos os metodos

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
     }
 
     // método a.
-    public int nOcorrencias(ArrayList l, Integer el) {
+    public int nOcorrencias(ArrayList<Integer> l, Integer el) {
         int conta = 0;
         for (int i = 0; i < l.size(); i++) {
             if (l.get(i) == el) {
@@ -15,20 +15,31 @@ public class Main {
         }
         return conta;
     }
-    
+
+    // método b.
+    public boolean hasRepeat(ArrayList<Integer> l) {
+        for (int i = 0; i < l.size() - 1; i++) {
+            if (l.get(i) == l.get(i + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     // método c.
-    public int nroRepeat(ArrayList l) {
-    int conta = 0;
-    for (int i = 0; i < l.size() - 1; i++) {
-    if (l.get(i) == l.get(i + 1)) {
-    conta++;
+    public int nroRepeat(ArrayList<Integer> l) {
+        int conta = 0;
+        for (int i = 0; i < l.size() - 1; i++) {
+            if (l.get(i) == l.get(i + 1)) {
+                conta++;
+            }
+        }
+        return conta;
     }
-    }
-    return conta;
-    }    
+
     // método d.
-    public ArrayList listRepeat(ArrayList l) {
-        ArrayList result = new ArrayList<>();
+    public ArrayList<Integer> listRepeat(ArrayList<Integer> l) {
+        ArrayList<Integer> result = new ArrayList<>();
         for (int i = 0; i < l.size(); i++) {
             for (int j = i + 1; j < l.size(); j++) {
                 if (l.get(i).equals(l.get(j)) && !result.contains(l.get(i))) {
@@ -39,18 +50,25 @@ public class Main {
         return result;
     }
 
-
+    // método e.
+    public ArrayList union(ArrayList l1, ArrayList l2) {
+        ArrayList result = new ArrayList<>(l1);
+        for (int i = 0; i < l2.size(); i++) {
+            if (!result.contains(l2.get(i))) {
+                result.add(l2.get(i));
+            }
+        }
+        return result;
+    }
 
     // método f.
-    public ArrayList intersect(ArrayList l1, ArrayList l2) {
-    ArrayList result = new ArrayList<>();
-    for (int i = 0; i < l1.size(); i++) {
-    if (l2.contains(l1.get(i)) && !result.contains(l1.get(i))) {
-    result.add(l1.get(i));
-    }
-    }
-    return result;
+    public ArrayList<Integer> intersect(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < l1.size(); i++) {
+            if (l2.contains(l1.get(i)) && !result.contains(l1.get(i))) {
+                result.add(l1.get(i));
+            }
+        }
+        return result;
     }
 }
-    
-
