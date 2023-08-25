110bce83e543aaa7512530aac027928246ee97e5
Athos Enderle Puna <129790032+Athosep08@users.noreply.github.com>
2023-08-25 18:10:36 -0300

    commit athos

--- Main.java
+++ Main.java
@@ -15,7 +15,17 @@ public class Main {
         }
         return conta;
     }
-
+    
+    // método c.
+    public int nroRepeat(ArrayList l) {
+    int conta = 0;
+    for (int i = 0; i < l.size() - 1; i++) {
+    if (l.get(i) == l.get(i + 1)) {
+    conta++;
+    }
+    }
+    return conta;
+    }    
     // método d.
     public ArrayList listRepeat(ArrayList l) {
         ArrayList result = new ArrayList<>();
@@ -29,4 +39,18 @@ public class Main {
         return result;
     }
 
+
+
+    // método f.
+    public ArrayList intersect(ArrayList l1, ArrayList l2) {
+    ArrayList result = new ArrayList<>();
+    for (int i = 0; i < l1.size(); i++) {
+    if (l2.contains(l1.get(i)) && !result.contains(l1.get(i))) {
+    result.add(l1.get(i));
+    }
+    }
+    return result;
+    }
 }
+    
+
