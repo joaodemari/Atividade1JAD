71ccd96df0213cb50c885a7125a49cfd71924f4c
João Pedro Antoniazi Tonello <jpantoniazitonello@gmail.com>
2023-08-25 17:58:17 -0300

    Functions a and b added

--- Main.java
+++ Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
+
+    // método a.
+    public int nOcorrencias(ArrayList l, Integer el) {
+        int conta = 0;
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i) == el) {
+                conta++;
+            }
+        }
+        return conta;
+    }
+
+    // método d.
+    public ArrayList listRepeat(ArrayList l) {
+        ArrayList result = new ArrayList<>();
+        for (int i = 0; i < l.size(); i++) {
+            for (int j = i + 1; j < l.size(); j++) {
+                if (l.get(i).equals(l.get(j)) && !result.contains(l.get(i))) {
+                    result.add(l.get(i));
+                }
+            }
+        }
+        return result;
+    }
+
 }
